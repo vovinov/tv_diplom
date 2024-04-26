7afbc7cc13643efe4ce923d16ead5a068f168aab
vovinov <v.v.ovinov@yandex.ru>
2024-04-27 02:59:03 +0300

    Replace settings, add postgres db option

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-45#g3ur4v8i&m@^v*%%5b9vlc6%z!^v3_mkwgwatg25n4k89td"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "core.project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
